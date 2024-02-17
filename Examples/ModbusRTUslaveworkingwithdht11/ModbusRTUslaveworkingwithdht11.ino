2fdb9f545b658aa065a1ca8d06400ca99d28576a
Ishinder Singh <78218217+ishu22ji@users.noreply.github.com>
2024-02-17 10:56:01 +0530

    Add files via upload

---
+++ Examples/ModbusRTUslaveworkingwithdht11/ModbusRTUslaveworkingwithdht11.ino
@@ -0,0 +1,102 @@
+/*
+  ModbusRTUSlaveExample
+
+  This example demonstrates how to setup and use the ModbusRTUSlave library.
+  It is intended to be used with a second board running ModbusRTUMasterExample from the ModbusRTUMaster library.  
+  
+  Circuit:
+  - A pushbutton switch from pin 2 to GND
+  - A pushbutton switch from pin 3 to GND
+  - A LED from pin 5 to GND with a 330 ohm series resistor
+  - A LED from pin 6 to GND with a 330 ohm series resistor
+  - A LED from pin 7 to GND with a 330 ohm series resistor
+  - A LED from pin 8 to GND with a 330 ohm series resistor
+  - The center pin of a potentiometer to pin A0, and the outside pins of the potentiometer to 5V and GND
+  - The center pin of a potentiometer to pin A0, and the outside pins of the potentiometer to 5V and GND
+  
+  !!! If your board's logic voltage is 3.3V, use 3.3V instead of 5V; if in doubt use the IOREF pin !!!
+  
+  - Pin 10 to pin 11 of the master/client board
+  - Pin 11 to pin 10 of the master/client board
+  - GND to GND of the master/client board
+  
+  A schematic and illustration of the circuit is in the extras folder of the ModbusRTUSlave library.
+
+  - Pin 13 is set up as the driver enable pin. This pin will be HIGH whenever the board is transmitting.
+  
+  Created: 2023-07-22
+  By: C. M. Bulliner
+  Modified: 2023-07-29
+  By: C. M. Bulliner
+  
+*/
+
+#include <SoftwareSerial.h>
+#include <ModbusRTUSlave.h>
+#include "DHT.h"
+#define DHTPIN 5
+
+const byte ledPins[4] = {8, 7, 6, 5};
+const byte buttonPins[2] = {9,10};
+const byte potPins[2] = {A0, A1};
+
+const uint8_t rxPin = 3;
+const uint8_t txPin = 4;
+const uint8_t dePin = 2;
+
+SoftwareSerial mySerial(rxPin, txPin);
+ModbusRTUSlave modbus(mySerial, dePin); // serial port, driver enable pin for rs-485 (optional)
+
+bool coils[2];
+bool discreteInputs[2];
+uint16_t holdingRegisters[2];
+uint16_t inputRegisters[2];
+///////////////////////////////////////////////
+#define DHTTYPE DHT11  // for DHT 11
+DHT dht(DHTPIN, DHTTYPE);
+int tem=0;
+int hum=0;
+void setup() {
+  pinMode(ledPins[0], OUTPUT);
+  pinMode(ledPins[1], OUTPUT);
+  pinMode(ledPins[2], OUTPUT);
+  pinMode(ledPins[3], OUTPUT);
+  pinMode(buttonPins[0], INPUT_PULLUP);
+  pinMode(buttonPins[1], INPUT_PULLUP);
+  pinMode(potPins[0], INPUT);
+  pinMode(potPins[1], INPUT);
+  dht.begin();
+  Serial.begin(9600);
+
+  modbus.configureCoils(coils, 2);                       // bool array of coil values, number of coils
+  modbus.configureDiscreteInputs(discreteInputs, 2);     // bool array of discrete input values, number of discrete inputs
+  modbus.configureHoldingRegisters(holdingRegisters,2); // unsigned 16 bit integer array of holding register values, number of holding registers
+  modbus.configureInputRegisters(inputRegisters, 2);     // unsigned 16 bit integer array of input register values, number of input registers
+  modbus.begin(1, 19200);                                // slave id, baud rate, config (optional)
+  Serial.println("Modbus DHT11 Sensor Data");
+  delay(2000);
+  Serial.println("---------------------------------");
+}
+
+void loop() {
+    // Read sensor data
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  tem=t;
+  hum=h;
+  Serial.println(hum);
+  Serial.println(tem);
+  delay(200);
+  discreteInputs[0] = !digitalRead(buttonPins[0]);
+  discreteInputs[1] = !digitalRead(buttonPins[1]);
+  inputRegisters[0] = map(analogRead(potPins[0]), 0, 1023, 0, 255);
+  inputRegisters[1] = map(analogRead(potPins[1]), 0, 1023, 0, 255);
+  holdingRegisters[0]=hum;
+  holdingRegisters[1]=tem;
+  modbus.poll();
+
+  digitalWrite(ledPins[0], coils[0]);
+  digitalWrite(ledPins[1], coils[1]);
+  //analogWrite(ledPins[2], holdingRegisters[0]);
+  //analogWrite(ledPins[3], holdingRegisters[1]);
+}
